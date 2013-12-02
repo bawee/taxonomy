d4f21950af16e1fe67beb92e0ac9af3b1c2af952
bawee <bryan.wee@uqconnect.edu.au>
2013-12-02 18:55:19 +1000

    Updated retrieve_child_gis.pl to include gis at the level of the query. Also tidied up and improved readibility

--- retrieve_child_gis.pl
+++ retrieve_child_gis.pl
@@ -1,40 +1,48 @@
 #!/opt/local/bin/perl
+#Written by Bryan Wee 1 August 2012
+#updated by BW 2 Dec 2013
 
-# requires nodes.dmp and names.dmp in the working directory
+#This script takes a query phrase e.g. "Pseudomonas" or "Pseudomonas aeruginosa" or "Pseudomonas aeruginosa UCBP-PA14" and returns all GI numbers associated with ALL taxa beneath the query taxa.
+
+#Requirements:
+# requires an updated copy of nodes.dmp and names.dmp in the working directory
 
 use warnings;
 use strict;
-
 use Bio::DB::Taxonomy;
 
-my $idx_dir = './tmp';
+#USAGE:
+my $species = $ARGV[0] or die "USAGE: retrieve_child_gis.pl <species>\n\nWhere <species> is a query string such as species name or genus or family etc.\n\n"; #user defines species name in quotes e.g. "Pseudomonas aeruginosa"
 
-my %hash_of_child_taxids;
- 
-system "mkdir tmp";
+my %hash_of_child_taxids; #initialize hash that stores all the taxids
 
-my $species = $ARGV[0] or die "USAGE: xxx.pl <species>"; #user defines species name in quotes e.g. "Pseudomonas aeruginosa"
+my $idx_dir = './tmp'; #
+system "mkdir tmp"; #create directory for the local taxonomy db
 
-
+#this part initializes the database of the nodes and names used by Bio::DB::Taxonomy
 my ($nodefile,$namesfile) = ('nodes.dmp','names.dmp');
 my $db = new Bio::DB::Taxonomy(-source    => 'flatfile',
                                -nodesfile => $nodefile,
                                -namesfile => $namesfile,
                                -directory => $idx_dir);
 
-my $taxon = $db->get_taxon(-name => $species);
+my $taxon = $db->get_taxon(-name => $species) or die "\nInvalid query, please try again or use the scientific name...\n\n"; #retrieve taxon object from the local database using user query phrase. Fails if query is invalid
 
-my $node = $db->get_Taxonomy_Node(-taxonid => $taxon->id); #this pulls out the taxid from the taxon specefied in the line above
-print $node->id, " ", $node->scientific_name, " ", $node->rank, "\n";
-get_descendents($node->id);
+my $node = $db->get_Taxonomy_Node(-taxonid => $taxon->id); #this line pulls out the TaxID of the taxon object from the line above
 
+$hash_of_child_taxids{$taxon->id} = $taxon->scientific_name; #this puts in the parent node/taxid into the hash. Previously this step was skipped resulting in GI numbers tied to the parent taxid to be missed. Added by BW 2.12.2013
 
-#print join("\n", @array_of_child_taxids);
+print "The query phrase matches: TaxID ", $node->id, " (", $node->scientific_name, "), ", $node->rank, "\n\n"; #Prints out the taxID, scientific name and rank of the query
+
+print "The taxa/taxon under the query taxon is/are:\n";
+get_descendents($node->id); #This runs the main recursive subroutine define below
 
 my $index;
 
+$species =~ s/\s/_/g;
 open FILE, "gi_taxid_prot.dmp" or die $!;
-open WRITE, ">>$species.gi_list" or die $!;
+open WRITE, ">$species.gi_list" or die $!;
+print "\nCollecting GI numbers.... (will take some time)\n";
 
 while (my $line = <FILE>)	{ #for each line in gi_taxid_prot.dmp
 
@@ -56,18 +64,17 @@ while (my $line = <FILE>)	{ #for each line in gi_taxid_prot.dmp
 close FILE;
 close WRITE;
 
+print "\nList of GI numbers associated with all taxa under $species have been printed to $species.gi_list\n";
+
 exit;
 
 
-
-
-sub get_descendents #This subroutine recursively searches for taxids below the given taxid until no taxids are found.
+sub get_descendents#This subroutine recursively searches for taxids below the given taxid until no further taxids are found.
 {
 
-
-my $taxonid = $_[0];
-my $node = $db->get_Taxonomy_Node(-taxonid => $taxonid);
-my @extant_children = $db->each_Descendent($node);
+my $taxonid = $_[0]; #receive the id of parent
+my $node = $db->get_Taxonomy_Node(-taxonid => $taxonid); #get the node object from the local taxonomy db in ./tmp
+my @extant_children = $db->each_Descendent($node); # also possible to use the method get_all_Descendents
 
 if (@extant_children)	#checks if @extant_children is filled
 	{  
@@ -78,7 +85,7 @@ if (@extant_children)	#checks if @extant_children is filled
 			#print "id is ", $child->id, "\n"; # NCBI taxa id
     		#print "rank is ", $child->rank, "\n"; # e.g. species
     		print $child->scientific_name, "\n"; # scientific name
-			$hash_of_child_taxids{$child->id} = $child->scientific_name;
+			$hash_of_child_taxids{$child->id} = $child->scientific_name; #creates a hash with format taxid => scientific_name
 			#push(@array_of_child_taxids, $child->id); #previously used an array to store taxids. Updated to use hash for faster access
 		}
 	}
